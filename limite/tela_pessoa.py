23037512efe46399548f74143754e3ccf1bf715e
Giulia Angeli <giuliaangelioliveira@gmail.com>
2022-02-01 21:08:16 -0300

    Adicionei na tela_pessoa as funções de selecionar qual tipo de pessoa (adm ou usuário), imprimir menu, inputar dados, fornecer dados da pessoa que quer acessar as informações

---
+++ limite/tela_pessoa.py
@@ -0,0 +1,60 @@
+from limite.tela_abstrata import *
+
+class TelaPessoa():
+
+    def adm_ou_usuario(self):
+        
+        cabecalho('CADASTRO DE...')
+        opcoes = ['[1] Administrador','[2] Usuário']
+        
+        for item in opcoes:
+            print(item)
+
+        print(linha())
+
+        opcao = leiaInt('Digite sua opção: ')
+        return opcao
+
+    def tela_opcoes(self):
+
+        opcoes = ['[1] Incluir','[2] Alterar', '[3] Excluir', '[4] Listar', '[0] Retornar']
+        
+        for item in opcoes:
+            print(item)
+
+        print(linha())
+
+        opcao = leiaInt('Sua opção: ')
+        return opcao
+
+    def pega_dados(self):
+
+        cabecalho('Insira os dados abaixo')
+
+        nome = input("Nome: ")
+        cpf = input("CPF: ")
+        telefone = input("Telefone: ")
+        endereco = input("Endereço: ")
+        email = input("E-mail: ")
+        senha = input("Senha: ")
+
+    def pega_dado_adm(self):
+
+        salario = input("Salário: ")
+
+    def mostra_pessoa(self, dados_pessoa):
+        print("NOME: ", dados["nome"])
+        print("CPF: ", dados["cpf"])
+        print("TELEFONE: ", dados["telefone"])
+        print("ENDEREÇO: ", dados["endereco"])
+        print("E-MAIL: ", dados["email"])
+        print("SENHA: ", dados["senha"])
+
+    def seleciona_pessoa(self):
+        cpf = input("Digite seu CPF: ")
+        senha = input("Digite a sua senha: ")
+
+    
+
+        
+
