3485432212003b510581d5aa0480e09c37123c7a
Giulia Angeli <giuliaangelioliveira@gmail.com>
2022-02-03 21:20:38 -0300

    criei as telas personalizadas para restringir o acesso do sistema de forma que quem não tem login consiga se cadastrar pra entrar e que as pessoas com login consigam entrar já com o menu personalizado de acordo com sua função no site

--- limite/tela_sistema.py
+++ limite/tela_sistema.py
@@ -1,12 +1,51 @@
+from limite.tela_abstrata import *
 class TelaSistema:
     #fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
-    def tela_opcoes(self):
-        print("-------- Sis ---------")
-        print("Escolha sua opcao")
-        print("1 - CORES")
-        print("2 - TAMANHOS")
-        print("3 - CATEGORIAS")
-        print("4 - PRODUTOS")
-        print("0 - Finalizar sistema")
-        opcao = int(input("Escolha a opcao:"))
+    def tela_inicial(self):
+        cabecalho('BEM-VINDO(A)')
+        opcoes = ['[1] Fazer Login','[2] Criar uma conta']
+        
+        for item in opcoes:
+            print(item)
+
+        print(linha())
+
+        opcao = leiaInt('Digite sua opção: ')
         return opcao
+
+    def tela_login(self):
+        cabecalho('ESCOLHA UMA OPÇÃO')
+        opcoes = ['[1] Login Administrador','[2] Login Cliente']
+        
+        for item in opcoes:
+            print(item)
+
+        print(linha())
+
+        opcao = leiaInt('Digite sua opção: ')
+        return opcao
+        
+    def tela_opcoes_adm(self):
+        
+        cabecalho('ESCOLHA UMA OPÇÃO')
+        opcoes = ['[1] Pessoas','[2] Produtos','[3] Histórico', '[4] Finalizar Sessão']
+        
+        for item in opcoes:
+            print(item)
+
+        print(linha())
+
+        opcao = leiaInt('Digite sua opção: ')
+
+
+    def tela_opções_usuario(self):
+        
+        cabecalho('ESCOLHA UMA OPÇÃO')
+        opcoes = ['[1] Ir as Compras','[2] Histórico de Compras','[3] Dados Pessoais', '[4] Finalizar Sessão']
+        
+        for item in opcoes:
+            print(item)
+
+        print(linha())
+
+        opcao = leiaInt('Digite sua opção: ')
