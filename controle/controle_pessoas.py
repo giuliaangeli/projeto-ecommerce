50b80a240dc7aa37f1fa1b476416ea26e28b3436
Giulia Angeli <giuliaangelioliveira@gmail.com>
2022-02-03 21:22:58 -0300

    eu mudei a função confere_login para que ela utilize dos dados captados pela tela na função 'pega_dados_login', tbm coloquei a função 'instancia_pessoas' que adiciona 2 adms na lista de adms e 2 usuários na lista de usuário assim que o programa inicia

--- controle/controle_pessoas.py
+++ controle/controle_pessoas.py
@@ -6,72 +6,66 @@ from limite.tela_abstrata import *
 
 class ControladorPessoa():
     
-    def __init__(self, controlador_controladores):
+    def __init__(self, controlador_sistema):
         self.__adms = []
         self.__usuarios = []
         self.__tela_pessoa = TelaPessoa()
-        self.__controlador_controladores = controlador_controladores
+        self.__controlador_sistema = controlador_sistema
     
     #condere pelo cpf informado se o usuario ou adm já está nas listas
-    def confere_pessoa_cpf(self, tipo_pessoa, cpf):
+    def confere_adm_cpf(self, cpf):
 
-        #se for adm
-        if tipo_pessoa == 1:
             for adm in self.__adms:
                 if (adm.cpf == cpf):
                     return adm
             return None
         
-        #se for usuario
-        else:
+    def confere_usuario_cpf(self, cpf):
+
             for usuario in self.__usuarios:
                 if (usuario.cpf == cpf):
                     return usuario
             return None
 
     #confere pelo e-mail e senha informados se o usuario ou adm está nas listas
-    def confere_pessoa(self, tipo_pessoa: int, email: str, senha: str):
-        
+    def confere_login(self, tipo_pessoa: int):
+        dados_login = self.__tela_pessoa.pega_dados_login()
+
         #se for adm
         if tipo_pessoa == 1:
             for adm in self.__adms:
-                if(adm.email == email) and (adm.senha == senha):
+                if(adm.email == dados_login['email']) and (adm.senha == dados_login['senha']):
                     return adm
             return None
 
         #se for usuário
         else:
             for usuario in self.__usuarios:
-                if(usuario.email == email) and (usuario.senha == senha):
+                if(usuario.email == dados_login['email']) and (usuario.senha == dados_login['senha']):
                     return usuario
             return None
+
+    def incluir_usuario(self):
+
+        dados_novo_usuario = self.__tela_pessoa.pega_dados_usuario()
+        usuario = self.confere_usuario_cpf(dados_novo_usuario["cpf"])
+
+        if usuario == None:
+            novo_usuario = Usuario(dados_novo_usuario["nome"], dados_novo_usuario["cpf"], dados_novo_usuario["telefone"], dados_novo_usuario["endereco"], dados_novo_usuario["email"], dados_novo_usuario["senha"])
+            self.__usuarios.append(novo_usuario)
+        else:
+            print('Esse CPF já está cadastrado')
         
-    def incluir_pessoa(self):
+    def incluir_adm(self):
+        dados_novo_adm = self.__tela_pessoa.pega_dado_adm()
+        adm = self.confere_adm_cpf(dados_novo_adm["cpf"])
 
-        tipo_pessoa = self.__tela_pessoa.adm_ou_usuario
-
-        if tipo_pessoa == 1:
-            cpf = self.__tela_pessoa.pega_dado_adm["cpf"]
-            adm = self.confere_pessoa_cpf(tipo_pessoa, cpf)
-
-            if adm == None:
-                dados_adm = self.__tela_pessoa.pega_dado_adm()
-                novo_adm = Adm(dados_adm["nome"], dados_adm["cpf"], dados_adm["telefone"], dados_adm["endereco"], dados_adm["email"], dados_adm["senha"], dados_adm["salario"])
-                self.__adms.append(novo_adm)
-
-            else:
-                print('Esse CPF já está cadastrado')
+        if adm == None:
+            novo_adm = Adm(dados_novo_adm["nome"], dados_novo_adm["cpf"], dados_novo_adm["telefone"], dados_novo_adm["endereco"], dados_novo_adm["email"], dados_novo_adm["senha"], dados_novo_adm["salario"])
+            self.__adms.append(novo_adm)
 
         else:
-            cpf = self.__tela_pessoa.pega_dados_usuario["cpf"]
-            usuario = self.confere_pessoa(tipo_pessoa, cpf)
-
-            if usuario == None:
-                dados_usuario = self.__tela_pessoa.pega_dados_usuario()
-                novo_usuario = Usuario(dados_usuario["nome"], dados_usuario["cpf"], dados_usuario["telefone"], dados_usuario["endereco"], dados_usuario["email"], dados_usuario["senha"])
-                self.__usuarios.append(novo_usuario)
-            else:
-                print('Esse CPF já está cadastrado')
+            print('Esse CPF já está cadastrado')
 
     def excluir_pessoa(self):
         tipo_pessoa = self.__tela_pessoa.adm_ou_usuario
@@ -152,4 +146,27 @@ class ControladorPessoa():
 
             if usuario is not None:
                 cabecalho('SUAS INFORMAÇÕES')
-                self.__tela_pessoa.mostra_usuario()
+                self.__tela_pessoa.mostra_usuario(usuario)
+
+    def retornar(self):
+        self.__controlador_sistema.abre_tela()
+
+    def abre_tela(self):
+
+        lista_opcoes = {1: self.incluir_pessoa, 2: self.alterar_pessoa, 3: self.listar_dados, 4: self.excluir_pessoa, 0: self.retornar}
+
+        continua = True
+        while continua:
+            lista_opcoes[self.__tela_pessoa.tela_opcoes()]()
+    
+    def instancia_pessoas(self):
+        #Instanciando 2 adms na lista
+        adm1 = Adm('Giulia Angeli','09641787969','47997930839','João Pio Duarte Silva','giulia@gmail.com','1234','5000')
+        adm2 = Adm('Guilherme Ferreira','99900011199','13996893954','Lauro Linhares','guilherme@gmail.com','1234','5000')
+        self.__adms.append(adm1)
+        self.__adms.append(adm2)
+        #Instanciando 2 clientes na lista
+        usuario1 = Usuario('Giulia Angeli','09641787969','47997930839','João Pio Duarte Silva','giulia@gmail.com','1234')
+        usuario2 = Usuario('Guilherme Ferreira','99900011199','13996893954','Lauro Linhares','guilherme@gmail.com','1234')
+        self.__usuarios.append(usuario1)
+        self.__usuarios.append(usuario2)
