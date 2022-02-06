707606c9ccd15e1a30352c050968602504b940e4
GuilhermeCassFerreira <94665366+GuilhermeCassFerreira@users.noreply.github.com>
2022-02-06 10:19:59 -0300

    alterei os metodos incluir, excluir e alterar
    Para evitar repetições na lista

--- controle/controlador_cores.py
+++ controle/controlador_cores.py
@@ -16,40 +16,62 @@ class ControladorCores():
 
   def incluir_cor(self):
     dados_cor = self.__tela_cor.pega_dados_cor()
-    cor = Cor(dados_cor["nome"], dados_cor["codigo"])
-    self.__cores.append(cor)
+    nova_cor = self.confere_cor_nome(dados_cor["nome"])
+
+    if nova_cor == None:
+      cor = Cor(dados_cor["nome"])
+      self.__cores.append(cor)
+      print('Cor foi selecionada com sucesso!')
+    else:
+      print('Essa cor já está cadastrado')
 
   def alterar_cor(self):
-    self.lista_cor()
-    codigo_cor = self.__tela_cor.seleciona_cor()
-    cor = self.pega_cor_por_codigo(codigo_cor)
+    print("Digite o nome da cor que você deseja alterar")
+    corAntiga = input().upper()
+    print("Digite o nome da cor pelo qual você deseja substituir")
+    corNova = input().upper()
+    verefica1 = False
+    verefica = False
+    for cor in self.__cores:
+      if cor.nome == corAntiga:
+        verefica = True
+      if cor.nome == corNova:
+        verefica1 = True
+        print("A cor que você deseja alterar já se encontra na lista")
+    if verefica == True and verefica1 != True:
+      for cor in self.__cores:
+        if cor == corAntiga:
+          cor.nome = corNova
+          print("Cor alterada com sucesso")
+    if verefica == False:
+      print("A cor que deseja alterar não se encontra na lista de cores")
+        
 
-    if(cor is not None):
-      novos_dados_cor = self.__tela_cor.pega_dados_cor()
-      cor.nome = novos_dados_cor["nome"]
-      cor.codigo = novos_dados_cor["codigo"]
-      self.lista_cor()
-    else:
-      self.__tela_cor.mostra_mensagem("ATENCAO: Cor não existente")
 
-  # Sugestão: se a lista estiver vazia, mostrar a mensagem de lista vazia
+
+   
+
   def lista_cor(self):
     for cor in self.__cores:
-      self.__tela_cor.mostra_cor({"nome": cor.nome, "codigo": cor.codigo})
+      self.__tela_cor.mostra_cor({"nome": cor.nome})
 
   def excluir_cor(self):
-    self.lista_cor()
-    codigo_cor = self.__tela_cor.seleciona_cor()
-    cor = self.pega_cor_por_codigo(codigo_cor)
-
-    if(cor is not None):
-      self.__cores.remove(cor)
-      self.lista_cor()
-    else:
-      self.__tela_cor.mostra_mensagem("ATENCAO: Cor não existente")
+    nome = self.__tela_cor.seleciona_cor()
+    for cor in self.__cores:
+      if cor.nome == nome:
+        self.__cores.remove(cor)
+        print('COR removido!')
+      else:
+        print("ATENÇÃO: essa cor não está cadastrado")
 
   def retornar(self):
     self.__controlador_sistema.abre_tela()
+
+  def confere_cor_nome(self, nome):
+    for cor in self.__cores:
+      if (cor.nome == nome):
+        return cor
+    return None
 
   def abre_tela(self):
     lista_opcoes = {1: self.incluir_cor, 2: self.alterar_cor, 3: self.lista_cor, 4: self.excluir_cor, 0: self.retornar}
@@ -59,15 +81,16 @@ class ControladorCores():
       lista_opcoes[self.__tela_cor.tela_opcoes()]()
 
   def instancia_cor(self):
-    vermelho = Cor('vermelho',1)
-    laranja = Cor('laranja',2)
-    rosa = Cor('rosa',3)
-    amarelo = Cor('amarelo ',4)
-    verde = Cor('verde ',5)
+    vermelho = Cor('VERMELHO')
+    laranja = Cor('LARANJA')
+    rosa = Cor('ROSA')
+    amarelo = Cor('AMARELO ')
+    verde = Cor('VERDE ')
     
     self.__cores.append(vermelho)
     self.__cores.append(laranja)
     self.__cores.append(rosa)
     self.__cores.append(amarelo)
     self.__cores.append(verde)
-    
+
+      
