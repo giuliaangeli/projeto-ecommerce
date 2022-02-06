0cebdb7b0a8860ab24c90e842c87de4176f677df
Giulia Angeli <giuliaangelioliveira@gmail.com>
2022-02-06 20:49:15 -0300

    eu retirei o conteudo das funções pq elas estão incorretas ainda, e mas criei a função que chama a tela de adm e a função que chama a tela do usuario

--- controle/controle_historico.py
+++ controle/controle_historico.py
@@ -9,32 +9,77 @@ class ControladorHistorico():
         self.__controlador_sistema = controlador_sistema
         self.__tela_historico = TelaHistorico()
     
-    def incluir_historico(self, codigo, usuario):
-        produto = self.__controlador_sistema.controlador_produtos.selecionar_produto(codigo)
-        self.__historico["Cliente"] = usuario
-        self.__historico["Produto"] = produto
+    def incluir_historico(self):
+        #produto = self.__controlador_sistema.controlador_produtos.selecionar_produto(codigo)
+        # codigo_produto = pass
+        # usuario = pass
+        # self.__historico["Cliente"] = usuario
+        # self.__historico["Produto"] = produto
+        pass
 
-    def excluir_historico(self, usuario, produto):
-        for historico in self.__historico:
-            if historico["Cliente"] == usuario and historico["Produto"] == produto:
-                self.__historico.remove(historico)
-                print("\nA venda foi removida do histórico com sucesso!\n")
+    def excluir_historico(self):
+        # usuario = pass
+        # produto = pass
+        # for historico in self.__historico:
+        #     if historico["Cliente"] == usuario and historico["Produto"] == produto:
+        #         self.__historico.remove(historico)
+        #         print("\nA venda foi removida do histórico com sucesso!\n")
 
-            else:
-                print("\nATENÇÃO: A venda desse produto para esse cliente não está no histórico\n")
+        #     else:
+        #         print("\nATENÇÃO: A venda desse produto para esse cliente não está no histórico\n")
+        pass
 
-    def alterar_historico_produto(self, usuario, produto, produto_novo):
-        for historico in self.__historico:
-            if historico["Cliente"] == usuario and historico["Produto"] == produto:
-                historico["Produto"] == produto_novo
-                print("\nHistorico alterado com sucesso!\n")
+    def alterar_historico_produto(self):
+        # usuario = pass
+        # produto = pass
+        # produto_novo = passar
+        # for historico in self.__historico:
+        #     if historico["Cliente"] == usuario and historico["Produto"] == produto:
+        #         historico["Produto"] == produto_novo
+        #         print("\nHistorico alterado com sucesso!\n")
 
-            else:
-                print("\nATENÇÃO: A venda não foi substituida pois essa venda não consta no histórico\n")
+        #     else:
+        #         print("\nATENÇÃO: A venda não foi substituida pois essa venda não consta no histórico\n")
+        pass
 
     def listar_historico_usuario(self, usuario):
-        for historico in self.__historico:
-            if historico["Cliente"] == usuario:
-                produto = historico["Produto"]
-                self.__tela_historico.imprime_historico_generico({"cor": produto.cor, "tamanho": produto.tamanho, "categoria": produto.categoria})
+        # usuario
+        # for historico in self.__historico:
+        #     if historico["Cliente"] == usuario:
+        #         produto = historico["Produto"]
+        #         self.__tela_historico.imprime_historico_generico({"cor": produto.cor, "tamanho": produto.tamanho, "categoria": produto.categoria})
+        pass
+    def listar_historico_simples(self):
+        pass
 
+    def listar_historico_personalizado(self):
+        pass
+
+    def voltar_menu_principal_adm(self, adm):
+        self.__controlador_sistema.controla_menu_principal_adm(adm)
+
+    def abrir_menu_filtro_adm(self, adm):
+
+        lista_opcoes = {1: self.listar_historico_simples, 2: self.listar_historico_personalizado, 3: self.abrir_menu_historico_adm, 4: self.__controlador_sistema.encerra_sistema}
+        
+        continua = True
+        while continua:
+            opcao_escolhida = self.__tela_historico.menu_opcao_filtro()
+            if opcao_escolhida == 1 or opcao_escolhida == 2:
+                lista_opcoes[opcao_escolhida]()
+            else:
+                lista_opcoes[opcao_escolhida]()
+
+    def abrir_menu_historico_adm(self, adm):
+         
+        lista_opcoes = {1: self.abrir_menu_filtro_adm, 2: self.alterar_historico_produto, 3: self.incluir_historico, 4: self.excluir_historico, 5: self.voltar_menu_principal_adm, 6: self.__controlador_sistema.encerra_sistema}
+        
+        continua = True
+        while continua:
+            opcao_escolhida = self.__tela_historico.menu_principal_adm()
+            if opcao_escolhida == 1 or opcao_escolhida == 5:
+                lista_opcoes[opcao_escolhida](adm)
+            else:
+                lista_opcoes[opcao_escolhida]()
+
+    
