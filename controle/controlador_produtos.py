109f3739400e64e1b076d349af9018486561e415
Giulia Angeli <giuliaangelioliveira@gmail.com>
2022-02-06 19:36:44 -0300

    adicionei a função 'alterar_produto' que ainda está vazia mas será criada verdadeiramente pelo guilherme. adicionei a função 'retornar_tela_adm_principal' que permite o adm voltar ao menu anterior. adicionei a função 'aabre_tela_produtos_adm' que chama a tela de produtos e posteriormente chama a função selecionada. cria as funções que chamam os menus de cor 'abre_menu_cor', tamanho 'abre_menu_tamanho' e categoria 'abre_menu_categoria'. adicionei a função 'retornar_tela_adm_produto' que retorna ao menu anterior da tela produtos e modifiquei a função 'menu_incluir_produto' que agora chamam as funções que abem o menu de cor, tamanho e categoria, alem de retornar ao menu principal dos adm e finalizar a sessão. adicionei a função 'usuario_compra_produto' que ainda está vazia. criei a função 'retorna_menu_principal_ususuario' que volta para o menu anterior do mesmo. e  por ultimo, criei a função 'abri_menu_usuario' que chama a tela do menu produto do usuario e chama a função de acordo com o que o usuário digitou

--- controle/controlador_produtos.py
+++ controle/controlador_produtos.py
@@ -54,8 +54,6 @@ class ControladorProdutos():
     else:
       self.__tela_produtos.mostra_mensagem("ATENCAO: Produto não existente")
 
-  
-
   def retornar(self):
     self.__controlador_sistema.abre_tela()
 
@@ -70,15 +68,67 @@ class ControladorProdutos():
       if (produto.tamanho == tamanho):
         return produto
     return None
+  
   def confere_produto_categoria(self, categoria):
     for produto in self.__produtos:
       if (produto.categoria == categoria):
         return produto
     return None
+  
+  def alterar_produto(self):
+    pass
 
-  def abre_tela(self):
-    lista_opcoes = {1: self.incluir_produto, 2: self.lista_produto, 3: self.excluir_produto,0: self.retornar}
+  def retornar_tela_adm_principal(self, adm):
+    self.__controlador_sistema.controla_menu_principal_adm(adm)
+
+  def abre_tela_produtos_adm(self, adm):
+    lista_opcoes = {1: self.menu_incluir_produto, 2: self.lista_produto, 3: self.alterar_produto, 4: self.excluir_produto, 5: self.retornar_tela_adm_principal, 6: self.__controlador_sistema.encerra_sistema}
 
     continua = True
     while continua:
-      lista_opcoes[self.__tela_produtos.tela_opcoes()]()
+      opcao_escolhida = self.__tela_produtos.tela_produtos_inicial_adm()
+      if opcao_escolhida == 6:
+        lista_opcoes[opcao_escolhida]()
+      else:
+        lista_opcoes[opcao_escolhida](adm)
+  
+  def abre_menu_cor(self, adm):
+    self.__controlador_sistema.controlador_cores.abre_tela(adm)
+
+  def abre_menu_tamanho(self, adm):
+    self.__controlador_sistema.controlador_tamanhos.abre_tela(adm)
+
+  def abre_menu_categoria(self, adm):
+    self.__controlador_sistema.controlador_categorias.abre_tela(adm)
+
+  def retornar_tela_adm_produto(self, adm):
+    self.abre_tela_produtos_adm(adm)
+
+  def menu_incluir_produto(self, adm):
+
+    lista_opcoes = {1: self.incluir_produto, 2: self.abre_menu_cor, 3: self.abre_menu_tamanho, 4: self.abre_menu_categoria, 5: self.retornar_tela_adm_produto, 6: self.__controlador_sistema.encerra_sistema}
+
+    continua = True
+    while continua:
+      opcao_escolhida = self.__tela_produtos.tela_produtos__adm()
+      if opcao_escolhida == 1 or opcao_escolhida == 6:
+        lista_opcoes[opcao_escolhida]()
+      else:
+        lista_opcoes[opcao_escolhida](adm)
+        
+  def usuario_compra_produto(self, usuario):
+    pass
+
+  def retorna_menu_principal_usuario(self, usuario):
+    self.__controlador_sistema.controla_menu_principal_usuario(usuario)
+
+  def abri_menu_usuario(self, usuario):
+    lista_opcoes = {1: self.lista_produto, 2: self.usuario_compra_produto, 3: self.retorna_menu_principal_usuario, 4: self.__controlador_sistema.encerra_sistema}
+    
+    continua = True
+    while continua:
+      opcao_escolhida = self.__tela_produtos.tela_produto_usuario()
+      if opcao_escolhida == 1 or opcao_escolhida == 4:
+        lista_opcoes[opcao_escolhida]()
+      else:
+        lista_opcoes[opcao_escolhida](usuario)
