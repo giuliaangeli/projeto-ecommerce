85df218ca5a9d857fad1b39d70c9eb7e78848b05
Giulia Angeli <giuliaangelioliveira@gmail.com>
2022-02-07 12:39:28 -0300

    na def troquei que self.__historico é uma lista, adicionei a função listar_quantidade_produtos_historico, confere_venda_historico, exlui_historico, alterar_historico_produto, listar_historico_usuario, relatorico_produos_iguais, conta_objetos, listar_historico_simples e arrumei o parametro que estava sendo passado errano no abir_menu_filtro_adm

--- controle/controle_historico.py
+++ controle/controle_historico.py
@@ -1,3 +1,4 @@
+from itertools import count
 from this import d
 from limite.tela_historico import TelaHistorico
 from entidade.historico import Historico
@@ -5,54 +6,80 @@ from entidade.historico import Historico
 class ControladorHistorico():
     
     def __init__(self, controlador_sistema):
-        self.__historico = dict()
+        self.__historico = []
         self.__controlador_sistema = controlador_sistema
         self.__tela_historico = TelaHistorico()
     
     def incluir_historico(self):
-        usuario = self.__controlador_sistema.controle_pessoas.confere_usuario_cpf_historico()
+        usuario = self.__controlador_sistema.controlador_pessoas.confere_usuario_cpf_historico()
         produto = self.__controlador_sistema.controlador_produtos.confere_produto_codigo()
         self.recebe_dados_venda(usuario, produto)
+
+    def listar_quantidade_produtos_historico(self, relatorio):
+        for venda in relatorio:
+            self.__tela_historico.imprime_historico_generico({"codigo": venda.produto.codigo,"nome_cor": venda.produto.cor.nome,"descricao_tamanho": venda.produto.tamanho.descricao,"tipo_categoria": venda.produto.categoria.tipo, "quantidade": venda.vezes})
 
     def recebe_dados_venda(self, usuario, produto):
         venda = Historico(usuario, produto)
         self.__historico.append(venda)
         print('A venda foi adicionada ao histórico!')
 
-    def excluir_historico(self):
-        # usuario = pass
-        # produto = pass
-        # for historico in self.__historico:
-        #     if historico["Cliente"] == usuario and historico["Produto"] == produto:
-        #         self.__historico.remove(historico)
-        #         print("\nA venda foi removida do histórico com sucesso!\n")
+    def confere_venda_historico(self):
+        usuario = self.__controlador_sistema.controle_pessoas.confere_usuario_cpf_historico()
+        produto = self.__controlador_sistema.controlador_produtos.confere_produto_codigo()
+        venda_para_validar = Historico(usuario, produto)
 
-        #     else:
-        #         print("\nATENÇÃO: A venda desse produto para esse cliente não está no histórico\n")
-        pass
+        for venda in self.__historico:
+            if venda == venda_para_validar:
+                return venda
+            else:
+                print('ATENÇÃO: A venda informada não consta no histórico, tente novamente!')
+                self.confere_venda_historico()
+
+    def excluir_historico(self):
+        venda_excluir = self.confere_venda_historico()
+        self.__historico.remove(venda_excluir)
 
     def alterar_historico_produto(self):
-        # usuario = pass
-        # produto = pass
-        # produto_novo = passar
-        # for historico in self.__historico:
-        #     if historico["Cliente"] == usuario and historico["Produto"] == produto:
-        #         historico["Produto"] == produto_novo
-        #         print("\nHistorico alterado com sucesso!\n")
-
-        #     else:
-        #         print("\nATENÇÃO: A venda não foi substituida pois essa venda não consta no histórico\n")
-        pass
+        venda_alterar = self.confere_venda_historico()
+        produto_alterado = self.__controlador_sistema.controlador_produtos.confere_produto_codigo()
+        venda_alterar.produto = produto_alterado
 
     def listar_historico_usuario(self, usuario):
-        # usuario
-        # for historico in self.__historico:
-        #     if historico["Cliente"] == usuario:
-        #         produto = historico["Produto"]
-        #         self.__tela_historico.imprime_historico_generico({"cor": produto.cor, "tamanho": produto.tamanho, "categoria": produto.categoria})
-        pass
+        historico_usuario = []
+        for venda in self.__historico:
+            if venda.usuario == usuario:
+                historico_usuario.append(venda.produto)
+
+        relatorio = self.relatorio_produtos_iguais(historico_usuario)
+        self.listar_quantidade_produtos_historico(relatorio)
+
+    def relatorio_produtos_iguais(self, lista_historico):
+        relatorio = list
+        lista_produtos = self.__controlador_sistema.controlador_produtos.retorna_lista_produtos()
+
+        for produto in lista_produtos:
+            vezes = self.conta_objetos(produto, lista_historico)
+            relatorio.append(produto, vezes)
+        return relatorio
+
+    def conta_objetos(self, produto, lista_produtos_comprados):
+        vezes = 0
+        for compra in lista_produtos_comprados:
+            if compra.cor.nome == produto.cor.nome and compra.tamanho.descricao == produto.tamanho.descricao and compra.categoria.tipo == produto.categoria.tipo:
+                vezes =+ 1
+
+        return vezes
+        
     def listar_historico_simples(self):
-        pass
+        lista_historico_produto = []
+
+        for venda in self.__historico:
+            produto_vendido = venda.produto
+            lista_historico_produto.append(produto_vendido)
+
+        relatorio = self.relatorio_produtos_iguais(lista_historico_produto)
+        self.listar_quantidade_produtos_historico(relatorio)
 
     def listar_historico_personalizado(self):
         pass
@@ -70,7 +97,7 @@ class ControladorHistorico():
             if opcao_escolhida == 1 or opcao_escolhida == 2:
                 lista_opcoes[opcao_escolhida]()
             else:
-                lista_opcoes[opcao_escolhida]()
+                lista_opcoes[opcao_escolhida](adm)
 
     def abrir_menu_historico_adm(self, adm):
          
